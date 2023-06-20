20121bc9deb1cf1d2fe38770638b80db5a403dc7
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-06-20 22:52:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-health-project">
+        <BrowserRouter basename="/bc_react_52_Reinghord">
           <App />
         </BrowserRouter>
       </PersistGate>
